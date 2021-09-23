be75f8b2e5eeb668392558323cbb3bf488e6c0ae
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-09-23 11:48:56 -0600

    Add test for all sort functions

--- LinkedListsTraining/LinkedListTraining_tests/Sort_tests.cs
+++ LinkedListsTraining/LinkedListTraining_tests/Sort_tests.cs
@@ -14,8 +14,8 @@ namespace LinkedListTraining_tests
         [TestMethod]
         public void Test_Can_Fail()
         {
-            //Assert.IsTrue(true);
-            Assert.Fail();
+            Assert.IsTrue(true);
+            //Assert.Fail();
         }
 
         [TestMethod]
@@ -111,6 +111,303 @@ namespace LinkedListTraining_tests
             var desired = new int[] { Int32.MinValue, - 1111, -1, 0, 2, 2, 2, 10, 44, 99999, Int32.MaxValue };
 
             Sort.Bubble(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+
+        // Selection Sort
+        [TestMethod]
+        public void Selection_Empty_ReturnsEmpty()
+        {
+            var arr = new int[] { };
+
+            Sort.Selection(arr);
+
+            Assert.AreEqual(arr.Length, 0);
+        }
+
+        [TestMethod]
+        public void Selection_OneItem_ReturnsItem()
+        {
+            var arr = new int[] { 1 };
+
+            Sort.Selection(arr);
+
+            Assert.AreEqual(arr.Length, 1);
+        }
+
+        [TestMethod]
+        public void Selection_TwoItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1 };
+            var desired = new int[] { 1, 2 };
+
+            Sort.Selection(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Selection_OddAmountOfItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1, 4, 8, 9 };
+            var desired = new int[] { 1, 2, 4, 8, 9 };
+
+            Sort.Selection(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Selection_EvenAmountOfItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1, 4, 8, 9, 6 };
+            var desired = new int[] { 1, 2, 4, 6, 8, 9 };
+
+            Sort.Selection(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Selection_ReversedList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            var desired = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Sort.Selection(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Selection_SimilarItemList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 5, 6, 5, 6, 5, 6, 5, 6 };
+            var desired = new int[] { 5, 5, 5, 5, 6, 6, 6, 6 };
+
+            Sort.Selection(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Selection_SpecialMergeSortNumberList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 38, 27, 43, 3, 9, 82, 10 };
+            var desired = new int[] { 3, 9, 10, 27, 38, 43, 82 };
+
+            Sort.Selection(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Selection_ListWithBounds_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 99999, Int32.MaxValue, Int32.MinValue, 10, 44, -1111, 0, 2, 2, 2, -1 };
+            var desired = new int[] { Int32.MinValue, -1111, -1, 0, 2, 2, 2, 10, 44, 99999, Int32.MaxValue };
+
+            Sort.Selection(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+
+        //Insertion Sort
+        [TestMethod]
+        public void Insertion_Empty_ReturnsEmpty()
+        {
+            var arr = new int[] { };
+
+            Sort.Insertion(arr);
+
+            Assert.AreEqual(arr.Length, 0);
+        }
+
+        [TestMethod]
+        public void Insertion_OneItem_ReturnsItem()
+        {
+            var arr = new int[] { 1 };
+
+            Sort.Insertion(arr);
+
+            Assert.AreEqual(arr.Length, 1);
+        }
+
+        [TestMethod]
+        public void Insertion_TwoItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1 };
+            var desired = new int[] { 1, 2 };
+
+            Sort.Insertion(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Insertion_OddAmountOfItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1, 4, 8, 9 };
+            var desired = new int[] { 1, 2, 4, 8, 9 };
+
+            Sort.Insertion(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Insertion_EvenAmountOfItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1, 4, 8, 9, 6 };
+            var desired = new int[] { 1, 2, 4, 6, 8, 9 };
+
+            Sort.Insertion(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Insertion_ReversedList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            var desired = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Sort.Insertion(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Insertion_SimilarItemList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 5, 6, 5, 6, 5, 6, 5, 6 };
+            var desired = new int[] { 5, 5, 5, 5, 6, 6, 6, 6 };
+
+            Sort.Insertion(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Insertion_SpecialMergeSortNumberList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 38, 27, 43, 3, 9, 82, 10 };
+            var desired = new int[] { 3, 9, 10, 27, 38, 43, 82 };
+
+            Sort.Insertion(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void Insertion_ListWithBounds_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 99999, Int32.MaxValue, Int32.MinValue, 10, 44, -1111, 0, 2, 2, 2, -1 };
+            var desired = new int[] { Int32.MinValue, -1111, -1, 0, 2, 2, 2, 10, 44, 99999, Int32.MaxValue };
+
+            Sort.Insertion(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+
+
+        [TestMethod]
+        public void MergeSort_Empty_ReturnsEmpty()
+        {
+            var arr = new int[] { };
+
+            Sort.MergeSort(arr);
+
+            Assert.AreEqual(arr.Length, 0);
+        }
+
+        [TestMethod]
+        public void MergeSort_OneItem_ReturnsItem()
+        {
+            var arr = new int[] { 1 };
+
+            Sort.MergeSort(arr);
+
+            Assert.AreEqual(arr.Length, 1);
+        }
+
+        [TestMethod]
+        public void MergeSort_TwoItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1 };
+            var desired = new int[] { 1, 2 };
+
+            Sort.MergeSort(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void MergeSort_OddAmountOfItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1, 4, 8, 9 };
+            var desired = new int[] { 1, 2, 4, 8, 9 };
+
+            Sort.MergeSort(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void MergeSort_EvenAmountOfItems_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 2, 1, 4, 8, 9, 6 };
+            var desired = new int[] { 1, 2, 4, 6, 8, 9 };
+
+            Sort.MergeSort(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void MergeSort_ReversedList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            var desired = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Sort.MergeSort(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void MergeSort_SimilarItemList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 5, 6, 5, 6, 5, 6, 5, 6 };
+            var desired = new int[] { 5, 5, 5, 5, 6, 6, 6, 6 };
+
+            Sort.MergeSort(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void MergeSort_SpecialMergeSortNumberList_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 38, 27, 43, 3, 9, 82, 10 };
+            var desired = new int[] { 3, 9, 10, 27, 38, 43, 82 };
+
+            Sort.MergeSort(arr);
+
+            CollectionAssert.AreEqual(arr, desired);
+        }
+
+        [TestMethod]
+        public void MergeSort_ListWithBounds_ReturnsItemsSorted()
+        {
+            var arr = new int[] { 99999, Int32.MaxValue, Int32.MinValue, 10, 44, -1111, 0, 2, 2, 2, -1 };
+            var desired = new int[] { Int32.MinValue, -1111, -1, 0, 2, 2, 2, 10, 44, 99999, Int32.MaxValue };
+
+            Sort.MergeSort(arr);
 
             CollectionAssert.AreEqual(arr, desired);
         }
